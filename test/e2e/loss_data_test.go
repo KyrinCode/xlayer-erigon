e3e87565ae607447beab324c3b386aff1a72ca93
jianguo <zjg555543@163.com>
2025-02-26 12:06:27 +0800

    update

--- test/e2e/loss_data_test.go
+++ test/e2e/loss_data_test.go
@@ -1,10 +1,20 @@
 package e2e
 
 import (
-	"github.com/stretchr/testify/require"
+	"context"
 	"io/ioutil"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/holiman/uint256"
+	"github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon/core/types"
+	"github.com/ledgerwatch/erigon/crypto"
+	"github.com/ledgerwatch/erigon/ethclient"
+	"github.com/ledgerwatch/erigon/test/operations"
+	"github.com/ledgerwatch/erigon/zkevm/encoding"
+	"github.com/stretchr/testify/require"
 )
 
 func TestModifyCode(t *testing.T) {
@@ -62,6 +72,7 @@ log.Info(fmt.Sprintf("CommitAndStart:%v,%v", batchState.batchNumber, blockNumber
 		t.Fatal("Error writing file:", err)
 	}
 }
+
 func TestRecoverCode(t *testing.T) {
 	filePath := "../../zk/stages/stage_sequence_execute.go"
 
@@ -88,4 +99,55 @@ log.Info(fmt.Sprintf("CommitAndStart:%v,%v", batchState.batchNumber, blockNumber
 	if err != nil {
 		t.Fatal("Error writing recovered file:", err)
 	}
+}
+
+func TestCheckSeqStop(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	for i := 0; i < 60; i++ {
+		batchNum, err := operations.GetBatchNumber()
+		require.NoError(t, err)
+		require.LessOrEqual(t, batchNum, uint64(5))
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func TestCheckVerify(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	if !testVerified {
+		return
+	}
+
+	ctx := context.Background()
+	auth, err := operations.GetAuth(operations.DefaultL2AdminPrivateKey, operations.DefaultL2ChainID)
+	require.NoError(t, err)
+	client, err := ethclient.Dial(operations.DefaultL2NetworkURL)
+	require.NoError(t, err)
+
+	from := common.HexToAddress(operations.DefaultL2AdminAddress)
+	to := common.HexToAddress(operations.DefaultL2NewAcc1Address)
+	nonce, err := client.PendingNonceAt(ctx, from)
+	require.NoError(t, err)
+	var tx types.Transaction = &types.LegacyTx{
+		CommonTx: types.CommonTx{
+			Nonce: nonce,
+			To:    &to,
+			Gas:   21000,
+			Value: uint256.NewInt(0),
+		},
+		GasPrice: uint256.NewInt(10 * encoding.Gwei),
+	}
+	privateKey, err := crypto.HexToECDSA(strings.TrimPrefix(operations.DefaultL2AdminPrivateKey, "0x"))
+	require.NoError(t, err)
+	signer := types.MakeSigner(operations.GetTestChainConfig(operations.DefaultL2ChainID), 1, 0)
+	signedTx, err := types.SignTx(tx, *signer, privateKey)
+	var txs []*types.Transaction
+	txs = append(txs, &signedTx)
+	_, err = operations.ApplyL2Txs(ctx, txs, auth, client, operations.VerifiedConfirmationLevel)
+	require.NoError(t, err)
 }
