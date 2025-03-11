3a06fc85d46f9264eddbbdd77e26821eca1c20e9
Dumi Loghin <dumiloghin@gmail.com>
2025-03-11 10:01:26 +0800

    improved prepare script using mdbx_copy

--- test/prepare-mainnet-split.sh
+++ test/prepare-mainnet-split.sh
@@ -5,12 +5,39 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
+# make sure you we have dbtools
+DBCPY=../build/bin/mdbx_copy
+if [ ! -e $DBCPY ]; then
+	cd ..
+	make db-tools
+	cd test
+	if [ ! -e $DBCPY ]; then
+		echo "dbtools (mdbx_copy) not found"
+		exit 1
+	fi
+fi
+
+# compile smt-db-split
+DBSPLIT=../cmd/smt-db-split/smt-db-split
+if [ ! -e $DBSPLIT ]; then
+	cd ../cmd/smt-db-split
+	go build
+	cd ../../test
+	if [ ! -e $DBSPLIT ]; then
+		echo "smt-db-split binary not found"
+		exit 1
+	fi
+fi
+
+SRC=$1
 DST="mainnet-split"
-echo "Copy from folder: $1"
+echo "Copy from folder: $SRC"
 echo "Copy to folder: $DST"
 
-cp -r $1 $DST
+mkdir -p $DST/seq/chaindata/
 mkdir -p $DST/seq/smt/
-cp $1/seq/chaindata/mdbx.* $DST/seq/smt/
 cp mdbx_opts/opts_chaindb.json $DST/seq/chaindata/
 cp mdbx_opts/opts_smt.json $DST/seq/smt/
+$DBCPY -c $SRC/seq/chaindata/mdbx.dat $DST/seq/chaindata/mdbx.dat
+cp $DST/seq/chaindata/mdbx.dat $DST/seq/smt/mdbx.dat
+$DBSPLIT $DST/seq
