78d85b8d392ac8a2fd2b18bcc63e29f7d21dcdac
cwbhhjl <cwbhhjl@gmail.com>
2025-03-12 22:01:13 +0800

    Refactor RemoveMinedTransactions method in TxPool to improve concurrency and state management

--- zk/txpool/pool_zk.go
+++ zk/txpool/pool_zk.go
@@ -334,43 +334,51 @@ func (p *TxPool) MarkForDiscardFromPendingBest(txHash common.Hash) {
 }
 
 func (p *TxPool) RemoveMinedTransactions(ctx context.Context, tx kv.Tx, blockGasLimit uint64, ids []common.Hash) error {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	cache := p._stateCache
 	toDelete := make([]*metaTx, 0)
 
+	p.lock.RLock()
 	p.all.ascendAll(func(mt *metaTx) bool {
 		for _, id := range ids {
 			if bytes.Equal(mt.Tx.IDHash[:], id[:]) {
 				toDelete = append(toDelete, mt)
-				switch mt.currentSubPool {
-				case PendingSubPool:
-					p.pending.Remove(mt)
-				case BaseFeeSubPool:
-					p.baseFee.Remove(mt)
-				case QueuedSubPool:
-					p.queued.Remove(mt)
-				default:
-					//already removed
-				}
 			}
 		}
 		return true
 	})
+	p.lock.RUnlock()
 
 	sendersWithChangedState := make(map[uint64]struct{})
 	for _, mt := range toDelete {
-		p.discardLocked(mt, Mined)
 		sendersWithChangedState[mt.Tx.SenderID] = struct{}{}
 	}
 
 	baseFee := p.pendingBaseFee.Load()
 
-	cacheView, err := cache.View(ctx, tx)
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	for _, mt := range toDelete {
+		switch mt.currentSubPool {
+		case PendingSubPool:
+			p.pending.Remove(mt)
+		case BaseFeeSubPool:
+			p.baseFee.Remove(mt)
+		case QueuedSubPool:
+			p.queued.Remove(mt)
+		default:
+			//already removed
+		}
+	}
+
+	for _, mt := range toDelete {
+		p.discardLocked(mt, Mined)
+	}
+
+	cacheView, err := p._stateCache.View(ctx, tx)
 	if err != nil {
 		return err
 	}
+
 	for senderID := range sendersWithChangedState {
 		nonce, balance, err := p.senders.info(cacheView, senderID)
 		if err != nil {
