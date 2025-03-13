65fada6487b439d32821d53212d88e698ee05bc7
Dumi Loghin <dumiloghin@gmail.com>
2025-03-13 12:06:37 +0800

    fix compiler issue in zk/legacy_executor_verifier/legacy_executor_verifier.go

--- zk/legacy_executor_verifier/legacy_executor_verifier.go
+++ zk/legacy_executor_verifier/legacy_executor_verifier.go
@@ -373,15 +373,6 @@ func (v *LegacyExecutorVerifier) VerifyWithMockExecutor(request *VerifierRequest
 			return verifierBundle, err
 		}
 
-    var txsmt kv.Tx = nil
-    if v.dbsmt != nil {
-		  txsmt, err = v.dbsmt.BeginRo(innerCtx)
-		  if err != nil {
-			  return verifierBundle, err
-		  }
-		  defer txsmt.Rollback()
-    }
-
 		witness, err := v.WitnessGenerator.GetWitnessByBlockRange(tx, txsmt, innerCtx, blockNumbers[0], blockNumbers[len(blockNumbers)-1], false, v.cfg.WitnessFull)
 		if err != nil {
 			return verifierBundle, err
