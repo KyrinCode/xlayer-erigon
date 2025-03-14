41e0c4f36476994ef46da2b2f2ed744862ca4d84
KyrinCode <kyrincode@qq.com>
2025-03-14 16:05:40 +0800

    fix block number metrics in the cases where txs are all aborted

--- zk/stages/stage_sequence_execute.go
+++ zk/stages/stage_sequence_execute.go
@@ -322,9 +322,6 @@ func sequencingBatchStep(
 			}
 		}
 
-		// For X Layer
-		metrics.GetLogStatistics().CumulativeCounting(metrics.BlockCounter)
-
 		header, parentBlock, err := prepareHeader(sdb.tx, blockNumber-1, batchState.blockState.getDeltaTimestamp(), batchState.getBlockHeaderForcedTimestamp(), batchState.forkId, batchState.getCoinbase(&cfg), cfg.chainConfig, cfg.miningConfig)
 		if err != nil {
 			return err
@@ -740,6 +737,7 @@ func sequencingBatchStep(
 		}
 
 		// For X Layer
+		metrics.GetLogStatistics().CumulativeCounting(metrics.BlockCounter)
 		// Count successful transactions
 		metrics.SeqTxCount.Add(float64(len(batchState.blockState.builtBlockElements.transactions)))
 		metrics.GetLogStatistics().CumulativeValue(metrics.TxCounter, int64(len(batchState.blockState.builtBlockElements.transactions)))
