3cbe769cabcdd4fbfa0d62533cb18ec6d8023697
Nauman-S <nauman843@hotmail.com>
2025-04-02 16:48:25 +0800

    ci_zkevm.yml

--- .github/scripts/update_config.sh
+++ .github/scripts/update_config.sh
@@ -1,20 +1,20 @@
 #! /bin/sh
 
-
-if ! command -v yq >/dev/null 2>&1; then
-  sudo apt update && sudo apt install -y yq
-fi
-
 CONFIG_FILES=(./test/config/test.erigon.seq.config.yaml)
 
 for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
 #  cat "$CONFIG_FILE"
   echo "Updating $CONFIG_FILE..."
+  sed -i '/^zkevm.executor-urls:/d' "$CONFIG_FILE"
+  sed -i '/^zkevm.executor-strict:/d' "$CONFIG_FILE"
+  sed -i '/^zkevm.witness-full:/d' "$CONFIG_FILE"
+  sed -i '/^zkevm.executor-mock:/d' "$CONFIG_FILE"
 
-  yq -i '.zkevm.executor-urls = "xlayer-executor:50071"' "$CONFIG_FILE"
-  yq -i '.zkevm.executor-strict = true' "$CONFIG_FILE"
-  yq -i '.zkevm.witness-full = false' "$CONFIG_FILE"
-  yq -i '.zkevm.executor-mock = false' "$CONFIG_FILE"
+  echo 'zkevm.executor-urls: "xlayer-executor:50071"' >> "$CONFIG_FILE"
+  echo 'zkevm.executor-strict: true' >> "$CONFIG_FILE"
+  echo 'zkevm.witness-full: false' >> "$CONFIG_FILE"
+  echo 'zkevm.executor-mock: false' >> "$CONFIG_FILE"
+
 
   echo "Finished updating $CONFIG_FILE."
 #  cat "$CONFIG_FILE"
