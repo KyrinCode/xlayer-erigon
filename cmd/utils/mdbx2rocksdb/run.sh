74eb42f1d17835e41c930d507f812059261e088f
zhe.yang <pandayt@163.com>
2025-04-10 15:32:17 +0800

    optimize GetForkIdBlock

--- cmd/utils/mdbx2rocksdb/run.sh
+++ cmd/utils/mdbx2rocksdb/run.sh
@@ -9,4 +9,4 @@ docker build -t mdbx2rocksdb -f cmd/utils/mdbx2rocksdb/Dockerfile .
 
 cd "$SCRIPT_DIR"
 
-docker run -v "$ROOT_DIR/test/data:/data" mdbx2rocksdb --mdbx /data/seq/chaindata --rocksdb /data/seq/chaindata_rocks --verbose
+docker run --rm -v "$ROOT_DIR/test/data:/data" mdbx2rocksdb --mdbx /data/seq/chaindata --rocksdb /data/seq/chaindata_rocks --verbose
