3ddf6b278977f3fce7034e7744fc4d863bbddc9a
louis.liu <louis.liu@okg.com>
2025-04-24 16:31:18 +0800

    delete duplicate code

--- smt/pkg/db/mdbx_cache.go
+++ smt/pkg/db/mdbx_cache.go
@@ -3,12 +3,10 @@ package db
 import (
 	"context"
 	"encoding/hex"
-	"github.com/benbjohnson/immutable"
 	"math/big"
-
-	"fmt"
 	"strings"
 
+	"github.com/benbjohnson/immutable"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon-lib/kv/membatch"
 	"github.com/ledgerwatch/erigon/smt/pkg/utils"
@@ -83,31 +81,9 @@ func (m *EriCacheDb) RollbackBatch() {
 	m.kvTxRoSMT = batch
 }
 
-func (m *EriCacheDb) GetLastRoot() (*big.Int, error) {
-	data, err := m.kvTxRoSMT.GetOne(TableStats, []byte(MetaLastRoot))
-	if err != nil {
-		return big.NewInt(0), err
-	}
-
-	if data == nil {
-		return big.NewInt(0), nil
-	}
-
-	return utils.ConvertHexToBigInt(string(data)), nil
-}
-
 func (m *EriCacheDb) SetLastRoot(r *big.Int) error {
 	v := utils.ConvertBigIntToHex(r)
 	return m.cacheTx.Put(TableStats, []byte(MetaLastRoot), []byte(v))
-}
-
-func (m *EriCacheDb) GetLastHeight() (uint64, error) {
-	data, err := m.kvTxRoSMT.GetOne(TableStats, []byte(MetaLastHeight))
-	if err != nil {
-		return 0, err
-	}
-
-	return utils.ConvertBytesToUint64(data)
 }
 
 func (m *EriCacheDb) SetLastHeight(blockHeight uint64) error {
@@ -115,39 +91,8 @@ func (m *EriCacheDb) SetLastHeight(blockHeight uint64) error {
 	return m.cacheTx.Put(TableStats, []byte(MetaLastHeight), []byte(v))
 }
 
-func (m *EriCacheDb) GetDepth() (uint8, error) {
-	data, err := m.kvTxRoSMT.GetOne(TableStats, []byte(MetaDepth))
-	if err != nil {
-		return 0, err
-	}
-
-	if data == nil {
-		return 0, nil
-	}
-
-	return data[0], nil
-}
-
 func (m *EriCacheDb) SetDepth(depth uint8) error {
 	return m.cacheTx.Put(TableStats, []byte(MetaDepth), []byte{depth})
-}
-
-func (m *EriCacheDb) Get(key utils.NodeKey, values *utils.NodeValue12) error {
-	k := utils.ArrayToHex(key[:])
-
-	data, err := m.kvTxRoSMT.GetOne(TableSmt, utils.UnsafeStringToBytes(k))
-	if err != nil {
-		return err
-	}
-
-	if data == nil || len(data) == 0 {
-		return nil
-	}
-
-	vConc := utils.ConvertHexToBigInt(utils.UnsafeBytesToString(data))
-	utils.ScalarToNodeValue(vConc, (*[12]*big.Int)(values))
-
-	return nil
 }
 
 func (m *EriCacheDb) Insert(key utils.NodeKey, value utils.NodeValue12) error {
@@ -166,25 +111,6 @@ func (m *EriCacheDb) Delete(key string) error {
 func (m *EriCacheDb) DeleteByNodeKey(key utils.NodeKey) error {
 	k := utils.ArrayToHex(key[:])
 	return m.cacheTx.Delete(TableSmt, utils.UnsafeStringToBytes(k))
-}
-
-func (m *EriCacheDb) GetAccountValue(key utils.NodeKey) (utils.NodeValue8, error) {
-	keyConc := utils.ArrayToScalar(key[:])
-	k := utils.ConvertBigIntToHex(keyConc)
-
-	data, err := m.kvTxRoSMT.GetOne(TableAccountValues, []byte(k))
-	if err != nil {
-		return utils.NodeValue8{}, err
-	}
-
-	if data == nil {
-		return utils.NodeValue8{}, nil
-	}
-
-	vConc := utils.ConvertHexToBigInt(string(data))
-	val := utils.ScalarToNodeValue8(vConc)
-
-	return val, nil
 }
 
 func (m *EriCacheDb) InsertAccountValue(key utils.NodeKey, value utils.NodeValue8) error {
@@ -212,21 +138,6 @@ func (m *EriCacheDb) DeleteKeySource(key utils.NodeKey) error {
 	return m.cacheTx.Delete(TableMetadata, keyConc.Bytes())
 }
 
-func (m *EriCacheDb) GetKeySource(key utils.NodeKey) ([]byte, error) {
-	keyConc := utils.ArrayToScalar(key[:])
-
-	data, err := m.kvTxRoSMT.GetOne(TableMetadata, keyConc.Bytes())
-	if err != nil {
-		return nil, err
-	}
-
-	if data == nil {
-		return nil, ErrNotFound
-	}
-
-	return data, nil
-}
-
 func (m *EriCacheDb) InsertHashKey(key utils.NodeKey, value utils.NodeKey) error {
 	keyConc := utils.ArrayToScalar(key[:])
 
@@ -240,40 +151,6 @@ func (m *EriCacheDb) DeleteHashKey(key utils.NodeKey) error {
 	return m.cacheTx.Delete(TableHashKey, keyConc.Bytes())
 }
 
-func (m *EriCacheDb) GetHashKey(key utils.NodeKey) (utils.NodeKey, error) {
-	keyConc := utils.ArrayToScalar(key[:])
-
-	data, err := m.kvTxRoSMT.GetOne(TableHashKey, keyConc.Bytes())
-	if err != nil {
-		return utils.NodeKey{}, err
-	}
-
-	if data == nil {
-		return utils.NodeKey{}, fmt.Errorf("hash key %x not found", keyConc.Bytes())
-	}
-
-	nv := big.NewInt(0).SetBytes(data)
-
-	na := utils.ScalarToArray(nv)
-
-	return utils.NodeKey{na[0], na[1], na[2], na[3]}, nil
-}
-
-func (m *EriCacheDb) GetCode(codeHash []byte) ([]byte, error) {
-	codeHash = utils.ResizeHashTo32BytesByPrefixingWithZeroes(codeHash)
-
-	data, err := m.kvTxRoChainDB.GetOne(kv.Code, codeHash)
-	if err != nil {
-		return nil, err
-	}
-
-	if data == nil {
-		return nil, fmt.Errorf("code hash %x not found", codeHash)
-	}
-
-	return data, nil
-}
-
 func (m *EriCacheDb) AddCode(code []byte) error {
 	codeHash := utils.HashContractBytecode(hex.EncodeToString(code))
 
@@ -285,58 +162,4 @@ func (m *EriCacheDb) AddCode(code []byte) error {
 	codeHashBytes = utils.ResizeHashTo32BytesByPrefixingWithZeroes(codeHashBytes)
 
 	return m.kvTxChainDB.Put(kv.Code, codeHashBytes, code)
-}
-
-func (m *EriCacheDb) PrintDb() {
-	err := m.kvTxRoSMT.ForEach(TableSmt, []byte{}, func(k, v []byte) error {
-		println(string(k), string(v))
-		return nil
-	})
-	if err != nil {
-		println(err)
-	}
-}
-
-func (m *EriCacheDb) GetDb() map[string][]string {
-	transformedDb := make(map[string][]string)
-
-	err := m.kvTxRoSMT.ForEach(TableSmt, []byte{}, func(k, v []byte) error {
-		hk := string(k)
-
-		vConc := utils.ConvertHexToBigInt(string(v))
-		val := utils.NewNodeValue12()
-		utils.ScalarToNodeValue(vConc, (*[12]*big.Int)(&val))
-
-		truncationLength := 12
-
-		allFirst8PaddedWithZeros := true
-		for i := 0; i < 8; i++ {
-			if !strings.HasPrefix(fmt.Sprintf("%016s", val[i].Text(16)), "00000000") {
-				allFirst8PaddedWithZeros = false
-				break
-			}
-		}
-
-		if allFirst8PaddedWithZeros {
-			truncationLength = 8
-		}
-
-		outputArr := make([]string, truncationLength)
-		for i := 0; i < truncationLength; i++ {
-			if i < len(val) {
-				outputArr[i] = fmt.Sprintf("%016s", val[i].Text(16))
-			} else {
-				outputArr[i] = "0000000000000000"
-			}
-		}
-
-		transformedDb[hk] = outputArr
-		return nil
-	})
-
-	if err != nil {
-		log.Error(err.Error())
-	}
-
-	return transformedDb
 }
